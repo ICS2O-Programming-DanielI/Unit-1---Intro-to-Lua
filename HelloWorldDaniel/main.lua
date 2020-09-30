1973d5f80b7b4de8f3f3722e4f6499bdde6f2f1b
s279796 <daniel.imperadeiro@ocsbstudent.ca>
2020-09-30 14:03:09 -0400

    Update main.lua

--- HelloWorldDaniel/main.lua
+++ HelloWorldDaniel/main.lua
@@ -1,7 +1,31 @@
------------------------------------------------------------------------------------------
+-- Title: HelloWorld
+-- Name: Daniel Imperadeiro
+-- Course: ICS2O/3C
+-- This program displays Hello to the simulator and to the command terminal.-----------------------------------------------------------------------------------------
 --
 -- main.lua
 --
 -----------------------------------------------------------------------------------------
 
--- Your code here
+-- print "Hello, World" to the command terminal
+print ("By:Daniel")
+print ("Hello Patrick!")
+
+-- hide the status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+--sets the background colour
+display.setDefault("background", 218/255, 165/255, 32/255)
+
+-- create a local variable
+local textObject
+local textObjectSignature
+
+-- displays text on the screen at the position x = 500 and y = 500 with
+-- a deafult font style and font size of 50
+textObject = display.newText( "By:Daniel", 500, 500, nil, 50)
+textObjectSignature = display.newText( "Hello, Patrick!", 500, 200, nil, 75)
+
+-- sets the color of the text
+textObject:setTextColor(72/255, 209/255, 204/255)
+textObjectSignature:setTextColor(250/255, 128/255, 114/255)
