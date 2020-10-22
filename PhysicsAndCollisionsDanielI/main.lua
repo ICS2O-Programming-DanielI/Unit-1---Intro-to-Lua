b97e606aab711f80eae667e37b1b891cedf7e6c0
s279796 <daniel.imperadeiro@ocsbstudent.ca>
2020-10-22 13:36:53 -0400

    added to animating images and physics

---
+++ PhysicsAndCollisionsDanielI/main.lua
@@ -0,0 +1,85 @@
+-- Title: PhysicsAndCollisions
+-- Name: Daniel Imperadeiro
+-- Course: ICS2O/3C
+-- This program...
+
+-- to remove status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+-- load physics
+local physics = require("physics")
+
+-- start physics
+physics.start()
+-----------------------------------------------------------------------------------------------
+--Objects
+-----------------------------------------------------------------------------------------------
+--Ground
+local ground = display.newImage("Images/ground.png", 500, 700)
+   --Change the width to be the same as the screen
+   ground.width = display.contentWidth
+
+   -- Add to physics
+   physics.addBody(ground, "static", {friction=0.5, bounce=0.3})
+
+--------------------------------------------------------------------------------------------------------
+--Beam
+local beam = display.newImage("Images/beam.png", 400, 600)
+
+	--set the x and y pos
+	beam.x = display.contentCenterX/5
+	beam.y = display.contentCenterY*1.65
+
+
+
+	-- rotate the beam -60 degrees so its on an angle
+	beam:rotate(45)
+
+	-- send it to the back layer
+	beam:toBack()
+
+	--add to physics
+	physics.addBody(beam, "static", {friction=0.5, bounce=0.3})
+
+--create bkg
+local bkg = display.newImage("Images/bkg.png", 0, 0)
+
+	--set the x and y pos
+	bkg.x = display.contentCenterX
+	bkg.y = display.contentCenterX
+
+
+
+	-- send to back
+	bkg:toBack()
+
+------------------------------------------------------------------------------------------------------------
+--FUNCTIONS
+--------------------------------------------------------------------------------------------
+
+-- create the first ball
+local function firstBall()
+	-- creating first ball
+	local ball1 = display.newImage("Images/super_ball.png", 0, 0)
+
+	-- add to physics
+	physics.addBody(ball1, {density=1.0, friction=0.5, bounce=0.3, radius=25})
+end
+
+---------------------------------------------------------------------------------------------
+
+local function secondBall()
+	-- create second ball
+	local ball2 = display.newImage("Images/")
+end
+
+
+
+
+
+
+----------------------------------------------------------------------------------------------------------------------
+--TIMER DELAYS - call each function after the given millisecond
+-----------------------------------------------------------------------------------------------------------
+timer.performWithDelay( 0, firstBall)
+timer.performWithDelay( 500, secondBall)
