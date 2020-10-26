a89e6adce4824f9ac982dfad39d02285e7a39d61
s279796 <daniel.imperadeiro@ocsbstudent.ca>
2020-10-26 13:46:40 -0400

    added to physics and collisions

--- PhysicsAndCollisionsDanielI/main.lua
+++ PhysicsAndCollisionsDanielI/main.lua
@@ -15,7 +15,7 @@ physics.start()
 --Objects
 -----------------------------------------------------------------------------------------------
 --Ground
-local ground = display.newImage("Images/ground.png", 500, 700)
+local ground = display.newImage("Images/ground.png", 510, 745)
    --Change the width to be the same as the screen
    ground.width = display.contentWidth
 
@@ -24,38 +24,39 @@ local ground = display.newImage("Images/ground.png", 500, 700)
 
 --------------------------------------------------------------------------------------------------------
 --Beam
-local beam = display.newImage("Images/beam.png", 400, 600)
+local beam = display.newImage("Images/beam.png", 0, 0)
 
 	--set the x and y pos
 	beam.x = display.contentCenterX/5
 	beam.y = display.contentCenterY*1.65
 
 
-
 	-- rotate the beam -60 degrees so its on an angle
-	beam:rotate(45)
+	beam:rotate(-60)
 
 	-- send it to the back layer
 	beam:toBack()
 
 	--add to physics
 	physics.addBody(beam, "static", {friction=0.5, bounce=0.3})
+	-- physics.addBody()
 
---create bkg
+-- create bkg
 local bkg = display.newImage("Images/bkg.png", 0, 0)
 
-	--set the x and y pos
+	-- set the x and y pos
 	bkg.x = display.contentCenterX
-	bkg.y = display.contentCenterX
+	bkg.y = display.contentCenterY
 
-
+	-- scale image 
+	bkg:scale(1.8, 1.8 )
 
 	-- send to back
 	bkg:toBack()
 
-------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------
 --FUNCTIONS
---------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------
 
 -- create the first ball
 local function firstBall()
@@ -66,20 +67,52 @@ local function firstBall()
 	physics.addBody(ball1, {density=1.0, friction=0.5, bounce=0.3, radius=25})
 end
 
----------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------------
 
 local function secondBall()
 	-- create second ball
-	local ball2 = display.newImage("Images/")
+	local ball2 = display.newImage("Images/super_ball.png", 0, 0)
+
+	-- adding to physics
+	physics.addBody(ball2, {density=1.0, friction=0.5, bounce=0.3, radius=12.5})
+
+	-- scale the image by 
+	ball2:scale(0.50, 0.50 )
 end
 
+--------------------------------------------------------------------------------------------------------------------
 
+local function thirdBall()
+	-- create third ball
+	local ball3 = display.newImage("Images/super_ball.png", 0, 0)
 
+	-- adding to physics
+	physics.addBody(ball3, {density=1.0, friction=0.5, bounce=0.3, radius=12.5})
 
+	-- scale the image by 
+	ball3:scale(2, 2 )
+end
 
+---------------------------------------------------------------------------------------------------------------------
+
+local function fourthBall()
+	-- create fourth ball
+	local ball4 = display.newImage("Images/super_ball.png", 0, 0)
+
+	-- adding to physics
+	physics.addBody(ball4, {density=1.0, friction=0.5, bounce=0.3, radius=12.5})
+
+	-- scale the image by 
+	ball4:scale(5, 5 )
+end
 
 ----------------------------------------------------------------------------------------------------------------------
 --TIMER DELAYS - call each function after the given millisecond
------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------
 timer.performWithDelay( 0, firstBall)
+
 timer.performWithDelay( 500, secondBall)
+
+timer.performWithDelay( 1000, thirdBall)
+
+timer.performWithDelay( 1500, fourthBall)
